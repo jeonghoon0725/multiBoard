6d3d216e7a1adf94ca2189b6cc1ab4334444de3a
JeongHoon.Han <yuria2005@gmail.com>
2024-12-05 01:58:18 +0900

    feat: 사용자 관리 2401205

---
+++ src/main/java/com/jhhan/multiBoard/User/UserController.java
@@ -0,0 +1,16 @@
+package com.jhhan.multiBoard.User;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/login")
+    public String loginForm(Model model) {
+        return "user/loginForm";
+    }
+}
